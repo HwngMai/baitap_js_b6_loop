173ffc4569cecbeb283f2d8357196088c64f90a8
Hung Mai <hwngmvi@gmail.com>
2022-07-07 19:33:51 +0700

    initiate

---
+++ main.js
@@ -0,0 +1,41 @@
+//Bài 1
+document.getElementById('btnBaiTap1').onclick = function (){
+    var tong = 0;
+    var soCanTim = '';
+    for (i = 0; (i + tong) <= 10000; i++){
+        tong += i;
+        soCanTim = i;
+    }
+    document.getElementById('ketQuaBaiTap1').innerHTML = 'Số cần tìm là: ' + soCanTim ;
+}
+//Bài 2
+document.getElementById('btnBaiTap2').onclick = function(){
+    var soX = document.getElementById('soX').value * 1;
+    var soN = document.getElementById('soN').value * 1;
+    var tong = 0;
+    for (i = 1; i <= soN ; i++){
+        tong += Math.pow(soX, i);
+    }
+    document.getElementById('ketQuaBaiTap2').innerHTML = tong;
+}
+//Bài 3
+document.getElementById('btnBaiTap3').onclick = function(){
+    var soM = document.getElementById('soM').value * 1;
+    var giaiThua = 1;
+    for ( i = 1; i <= soM; i++){
+        giaiThua *= i;
+    }
+    document.getElementById('ketQuaBaiTap3').innerHTML = giaiThua;
+}
+//Bài 4
+document.getElementById('btnBaiTap4').onclick = function(){
+    ketQua = '';
+    for ( i = 1 ; i <=10 ; i++){
+        if( i % 2 == 0){
+        ketQua += '<div class="bg-danger">' + 'div chẵn ' + Math.ceil(i/2) + '</div>';
+    }   else {
+        ketQua += '<div class="bg-primary">' + 'div lẻ ' + Math.ceil(i/2) + '</div>';
+    }
+    }
+    document.getElementById('ketQua').innerHTML = ketQua;
+}
